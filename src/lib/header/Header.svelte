9eae3e23d7ec1d83095f7bf2917ee33d4048cffc
eoghan <eoghan.firth@gmail.com>
2022-03-15 19:33:57 +0000

    styling

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,28 +1,31 @@
 <header>
-	<div class="cornerText">
+	<div class="headerheader">
 		<a href="https://eoghan.dev">eoghan.dev</a>
 	</div>
-	<a href="/">
-		<span class="title">DaoSplit</span>
-	</a>
-
-	<div class="corner">
-		<!-- TODO put something else here? github link? -->
+	<div class="titlebox">
+		<a href="/">
+			<span class="title">DaoSplit</span>
+		</a>
 	</div>
 </header>
 
 <style>
+	.headerheader {
+		padding-left: 5px;
+	}
 	header {
-		display: flex;
+		flex-direction: column;
 		justify-content: space-between;
 	}
+
+	.titlebox {
+		display: flex;
+		justify-content: center;
+	}
+
 	.title {
 		font-family: 'Times New Roman', Times, serif;
 		font-size: 90px;
-	}
-	.corner {
-		width: 3em;
-		height: 3em;
 	}
 
 	a:hover {
