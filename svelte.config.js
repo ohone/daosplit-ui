b06d45d5cb72c6c88bcad519324ebd1594f0ffb0
eoghan <eoghan.firth@gmail.com>
2022-03-19 10:39:57 +0000

    use adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,18 @@
-import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({ out: 'build' }),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		}
+		adapter: adapter({ 
+						// default options are shown
+						pages: 'build',
+						assets: 'build',
+						fallback: null,
+						precompress: false
+		 })
 	}
 };
 
