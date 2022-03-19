04d864864181f888e9595f01698800181cae0f2c
eoghan <eoghan.firth@gmail.com>
2022-03-19 11:33:56 +0000

    latest sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -4,15 +4,17 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
-
 	kit: {
-		adapter: adapter({ 
-						// default options are shown
-						pages: 'build',
-						assets: 'build',
-						fallback: null,
-						precompress: false
-		 })
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+		prerender:{
+			default: true
+		}
 	}
 };
 
